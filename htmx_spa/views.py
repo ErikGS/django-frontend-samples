17d1086b50dd529523091006fe0146d83202806e
ErikGS <erikgs@mixal.me>
2024-03-17 01:10:32 -0300

    Created HTMX views

--- htmx_spa/views.py
+++ htmx_spa/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+class HtmxSpaView(LoginRequiredMixin, TemplateView):
+    template_name = "htmx_spa/index.html"
+
+class HtmxFormsSpaView(LoginRequiredMixin, TemplateView):
+    template_name = "htmx_spa/forms_full.html"
