4e8556be06504ee53148a0af0164568469481414
ErikGS <erikgs@mixal.me>
2024-03-17 01:10:05 -0300

    Created default home view

---
+++ backend/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'index.html',
+        {
+            'title':'Django Frontend Samples',
+            'message':'This is a default SSR django page.',
+        }
+    )
