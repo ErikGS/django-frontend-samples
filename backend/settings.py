c8081af103161a43beb1c0c8bf2eb4e355db0ac4
ErikGS <erikgs@mixal.me>
2024-03-17 02:38:17 -0300

    added django-htmx package

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_htmx',
     "rest_framework",
     "svelte_spa",
     "htmx_spa",
